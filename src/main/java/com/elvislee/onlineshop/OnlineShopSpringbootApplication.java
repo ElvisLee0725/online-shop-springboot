bd71e5d7054207bbaa4602af0f6e94a6a7eb55b6
ElvisLee0725 <elvislee0725@gmail.com>
2022-03-14 21:54:58 -0700

    Initialize SpringBoot project

---
+++ src/main/java/com/elvislee/onlineshop/OnlineShopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.elvislee.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elvislee/onlineshop/OnlineShopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvislee.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
