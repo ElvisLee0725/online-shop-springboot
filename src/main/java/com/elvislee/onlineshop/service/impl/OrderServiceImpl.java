040c6e0e44b31d09f3fc1c950bffd096050e9363
ElvisLee0725 <elvislee0725@gmail.com>
2022-07-18 22:00:30 -0700

    Create order

---
+++ src/main/java/com/elvislee/onlineshop/service/impl/OrderServiceImpl.java
@@ -0,0 +1,51 @@
+package com.elvislee.onlineshop.service.impl;
+
+import com.elvislee.onlineshop.dao.OrderDao;
+import com.elvislee.onlineshop.dao.ProductDao;
+import com.elvislee.onlineshop.dto.BuyItem;
+import com.elvislee.onlineshop.dto.CreateOrderRequest;
+import com.elvislee.onlineshop.model.OrderItem;
+import com.elvislee.onlineshop.model.Product;
+import com.elvislee.onlineshop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Transactional
+    @Override
+    public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        int totalAmount = 0;
+        List<OrderItem> orderItemList = new ArrayList<>();
+
+        for(BuyItem buyItem : createOrderRequest.getBuyItemList()) {
+            Product product = productDao.getProductById(buyItem.getProductId());
+            int amount = buyItem.getQuantity() * product.getPrice();
+            totalAmount += amount;
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProductId(buyItem.getProductId());
+            orderItem.setQuantity(buyItem.getQuantity());
+            orderItem.setAmount(amount);
+
+            orderItemList.add(orderItem);
+        }
+
+        Integer orderId = orderDao.createOrder(userId, totalAmount);
+
+        orderDao.createOrderItems(orderId, orderItemList);
+
+        return orderId;
+    }
+}
